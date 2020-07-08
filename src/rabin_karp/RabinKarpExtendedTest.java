e0d73e98cf9af8635602c67d018601daa9351495
OlegBvG <borisov_og@mail.ru>
2020-07-08 23:15:54 +0300

    Algorithms with RabinKarp (not void)

--- src/rabin_karp/RabinKarpExtendedTest.java
+++ src/rabin_karp/RabinKarpExtendedTest.java
@@ -12,11 +12,10 @@ import org.junit.jupiter.api.Test;
 class RabinKarpExtendedTest {
 
   String txt = "Like the Naive Algorithm, Rabin-Karp algorithm also slides the pattern one by one. But unlike the Naive algorithm, Rabin Karp algorithm matches the hash value of the pattern with the hash value of current substring of text, and if the hash values match then only it starts matching individual characters. So Rabin Karp algorithm needs to calculate hash values for following strings.";
-
   String pat = "Rabin-Karp algorithm";
+  ArrayList numberPositionTest = new ArrayList();
   //    int q = 101; // Простое число
   int q = (int) (pow(2, 64) - 1); // Простое число
-  ArrayList numberPositionTest = new ArrayList();
 
 
   @BeforeEach
